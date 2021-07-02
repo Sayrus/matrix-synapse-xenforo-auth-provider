e5689fbcfa610ff0fefad58772234ec38d40bebe
Mathis Raguin <mathis@cri.epita.fr>
2021-07-02 15:35:05 +0200

    init: initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="xenforo_auth_provider",
+    version="0.0.1",
+    py_modules=['xenforo_auth_provider'],
+    description="Xenforo provider for Synapse",
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[],
+)
---
+++ xenforo_auth_provider.py
@@ -0,0 +1,37 @@
+import os
+import requests
+
+
+class XenforoAuthProvider(object):
+    def __init__(self, config, account_handler):
+        self.account_handler = account_handler
+
+        self.endpoint = config.endpoint
+
+        self.api_key = config.api_key or os.getenv('XF_API_KEY')
+        if not self.api_key:
+            raise RuntimeError('Missing API Key')
+
+    def get_supported_login_types(self):
+        return {'m.login.password': ('password',)}
+
+    async def check_3pid_auth(self, medium, address, password):
+        if medium != "email":
+            return None
+
+        payload = {'login': address, 'password': password}
+        headers = {'xf-api-key': self.api_key}
+        r = requests.post(self.endpoint + "/api/auth", data=payload, headers=headers)
+        if r.status_code != 200:
+            return False
+        r = r.json()
+        if r["success"] is not True:
+            return False
+
+        return r['user']['user_id']
+
+    @staticmethod
+    def parse_config(config):
+        if not config.endpoint:
+            raise Exception("Missing endpoint config")
+        return config
